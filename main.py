34001dac6ece23746a411face8581f1ed092903f
MazinSherif-E <mazin.sherif100@gmail.com>
2024-12-08 14:13:06 +0200

    edited main file to receive base router

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env") # one time in main file to make all files see it
+
+from routes import base
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Hello World!"
-    }
+app.include_router(base.base_router)
