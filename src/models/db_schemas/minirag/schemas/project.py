1a99b669fa25e080727e89822025f4ca467b8c0a
MazinSherif-E <mazin.sherif100@gmail.com>
2025-01-18 19:49:24 +0200

    edits in project scheam

--- src/models/db_schemas/minirag/schemas/project.py
+++ src/models/db_schemas/minirag/schemas/project.py
@@ -2,6 +2,7 @@ from .minirag_base import SQLAlchemyBase
 from sqlalchemy import Column, Integer, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+from sqlalchemy.orm import relationship
 
 class Project(SQLAlchemyBase):
     __tablename__ = "projects"
@@ -11,4 +12,5 @@ class Project(SQLAlchemyBase):
     project_created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     project_updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
 
-    
+    chunks = relationship("DataChunk", back_populates="project")
+    assets = relationship("Asset", back_populates="project")
