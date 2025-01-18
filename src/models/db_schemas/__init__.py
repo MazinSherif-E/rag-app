146f9cf1c47c98d396adf97433d49b8160de036d
MazinSherif-E <mazin.sherif100@gmail.com>
2025-01-18 19:51:05 +0200

    init file for db schemas

--- src/models/db_schemas/__init__.py
+++ src/models/db_schemas/__init__.py
@@ -1,3 +1,1 @@
-from .project import Project
-from .data_chunk import DataChunk, RetrievedDocument
-from .asset import Asset
+from .minirag.schemas import Project, DataChunk, RetrievedDocument, Asset
